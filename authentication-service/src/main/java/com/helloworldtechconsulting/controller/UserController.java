936b1daa742eb7be3d092b7adf538e48ae60cb37
Naveen Kumar <connect.naveenk@gmail.com>
2024-09-18 18:41:32 +0530

    Get user details fixes#1

--- authentication-service/src/main/java/com/helloworldtechconsulting/controller/UserController.java
+++ authentication-service/src/main/java/com/helloworldtechconsulting/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.helloworldtechconsulting.controller;
 
 import com.helloworldtechconsulting.model.ChangePasswordRequest;
+import com.helloworldtechconsulting.model.entities.User;
 import com.helloworldtechconsulting.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -27,7 +31,14 @@ public class UserController {
 
     @GetMapping("/details")
     public ResponseEntity<?> getDetails(Principal connectedUser) {
-        service.loadUserByUsername(connectedUser.getName());
-        return ResponseEntity.ok().build();
+        User user = (User) service.loadUserByUsername(connectedUser.getName());// this is working fine but we have to return the only required user details
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("id",user.getId());
+        response.put("email", user.getEmail());
+        response.put("firstname", user.getFirstname());
+        response.put("lastname", user.getLastname());
+
+        return ResponseEntity.ok(response);
     }
 }
