eea7ff16c24b78c71d2116b5e715e869b7d68664
SPS <sumitpalsingh007@gmail.com>
2025-11-17 20:32:06 +0530

    redis cache

--- order-service/src/main/java/com/helloworldtechconsulting/dto/ProductDto.java
+++ order-service/src/main/java/com/helloworldtechconsulting/dto/ProductDto.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
-public class ProductDto {
+public class ProductDto implements Serializable {
 
     String id;
     String name;
